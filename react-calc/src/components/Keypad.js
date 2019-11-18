cb4eacef89659befb633173e3f8d6d4e160aa3dc
Lauren Bullen <laurenbullen@comcast.net>
2019-11-17 20:52:11 -0800

    changed css

--- react-calc/src/components/Keypad.js
+++ react-calc/src/components/Keypad.js
@@ -9,25 +9,25 @@ class Keypad extends React.Component {
                 <button name='1' onClick={e => this.props.onClick(e.target.name)}>1</button>
                 <button name='2' onClick={e => this.props.onClick(e.target.name)}>2</button>
                 <button name='3' onClick={e => this.props.onClick(e.target.name)}>3</button>
-                <button name='+' onClick={e => this.props.onClick(e.target.name)}>+</button>
+                <button className='operator' name='+' onClick={e => this.props.onClick(e.target.name)}>+</button>
                 <br/>
 
                 <button name='4' onClick={event => this.props.onClick(event.target.name)}>4</button>
                 <button name='5' onClick={e => this.props.onClick(e.target.name)}>5</button>
                 <button name='6' onClick={e => this.props.onClick(e.target.name)}>6</button>
-                <button name='-' onClick={e => this.props.onClick(e.target.name)}>-</button>
+                <button className='operator' name='-' onClick={e => this.props.onClick(e.target.name)}>-</button>
                 <br/>
 
                 <button name='7' onClick={e => this.props.onClick(e.target.name)}>7</button>
                 <button name='8' onClick={e => this.props.onClick(e.target.name)}>8</button>
                 <button name='9' onClick={e => this.props.onClick(e.target.name)}>9</button>
-                <button name='*' onClick={e => this.props.onClick(e.target.name)}>*</button>
+                <button className='operator' name='*' onClick={e => this.props.onClick(e.target.name)}>*</button>
                 <br/>
 
                 <button name='.' onClick={e => this.props.onClick(e.target.name)}>.</button>
                 <button name='0' onClick={e => this.props.onClick(e.target.name)}>0</button>
                 <button name='=' onClick={e => this.props.onClick(e.target.name)}>=</button>
-                <button name='/' onClick={e => this.props.onClick(e.target.name)}>/</button>
+                <button className='operator' name='/' onClick={e => this.props.onClick(e.target.name)}>/</button>
                 <br />
                 <button className='clear' name='clear' onClick={e => this.props.onClick(e.target.name)}>Clear</button>
 
