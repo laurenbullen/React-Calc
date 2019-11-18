b493f7b05e35bf1325a240d14781f8c89bac55c2
Lauren Bullen <laurenbullen@comcast.net>
2019-11-17 16:55:45 -0800

    3 components added

---
+++ react-calc/src/components/Keypad.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Keypad extends React.Component {
+    render() {
+        return(
+            <div>Numbers Here</div>
+        )
+    }
+}
+
+export default Keypad
---
+++ react-calc/src/components/Results.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Results extends React.Component {
+    render(){
+        return (
+            <p>Results Here</p>
+        )
+    }
+}
+
+export default Results 
